271c79a45dec6f7e3a9e86cdc2ea82a7b3a07df5
Marcos Vidal <markos_ari@hotmail.com>
2024-08-10 13:39:42 -0300

    CLEAN original components and files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
+
+import { Box } from "@mui/material";
 import { Inter } from "next/font/google";
-import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,13 +11,13 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <Box component="html" lang="en">
+        <Box component="body" m={0} className={inter.className}>{children}</Box>
+    </Box>
   );
 }
