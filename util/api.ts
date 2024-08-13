00746d1f5ac04906b2d852a43dca897a8631c539
Marcos Vidal <markos_ari@hotmail.com>
2024-08-13 20:40:04 -0300

    ADD axios methods

---
+++ util/api.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const vars = {
+    REACT_APP_TMDB_BASE_URL_API:"https://api.themoviedb.org/3/",
+TMDB_API_KEY:"eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZTRlZTQzMWM3NTE0M2FjNGIwMzBmMTA1Mzc2NjIzYiIsIm5iZiI6MTcyMzU4MDg0Mi4xODM5NTEsInN1YiI6IjVmMmFkY2I2M2UwOWYzMDAzNjIwZTVkZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZiDhx3l_TlGa-kltGCx2n93KEaIzVscss5jVqe7stwQ"
+}
+
+const axiosInstance = axios.create({
+    baseURL: vars.REACT_APP_TMDB_BASE_URL_API,
+    params: { language: "es" },
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${vars.TMDB_API_KEY}`
+    }
+})
+
+export const getMovies = async () => {
+    const resp = await axiosInstance.get("/discover/movie")
+    return resp.data;
+}
+
+export const getGenres = async () => {
+    const resp = await axiosInstance.get("/genre/movie/list")
+    return resp.data;
+}
