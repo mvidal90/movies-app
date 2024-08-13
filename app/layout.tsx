657d0f04fa3e73ef6b0d24213bd9054ea44bc74f
Marcos Vidal <markos_ari@hotmail.com>
2024-08-12 21:03:04 -0300

    ADD redux toolkit configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import { ThemeProvider } from '@mui/material/styles';
 
 import Navbar from "@/components/navbar/Navbar";
 
+import StoreProvider from "./StoreProvider";
+
 import theme from "@/theme";
 
 export const metadata: Metadata = {
@@ -22,8 +24,10 @@ export default function RootLayout({
             <Box component="body" m={0}>
                 <ThemeProvider theme={theme}>
                     <CssBaseline />
-                    <Navbar />
-                    {children}
+                    <StoreProvider>
+                        <Navbar />
+                        {children}
+                    </StoreProvider>
                 </ThemeProvider>
             </Box>
         </Box>
