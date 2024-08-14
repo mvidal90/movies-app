bcd292736a2eb851687423d5036484a8533c239a
Marcos Vidal <markos_ari@hotmail.com>
2024-08-13 23:13:03 -0300

    FIX default Lang

--- util/api.ts
+++ util/api.ts
@@ -2,12 +2,12 @@ import axios from "axios";
 
 const vars = {
     REACT_APP_TMDB_BASE_URL_API:"https://api.themoviedb.org/3/",
-TMDB_API_KEY:"eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZTRlZTQzMWM3NTE0M2FjNGIwMzBmMTA1Mzc2NjIzYiIsIm5iZiI6MTcyMzU4MDg0Mi4xODM5NTEsInN1YiI6IjVmMmFkY2I2M2UwOWYzMDAzNjIwZTVkZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZiDhx3l_TlGa-kltGCx2n93KEaIzVscss5jVqe7stwQ"
+    TMDB_API_KEY:"eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZTRlZTQzMWM3NTE0M2FjNGIwMzBmMTA1Mzc2NjIzYiIsIm5iZiI6MTcyMzU4MDg0Mi4xODM5NTEsInN1YiI6IjVmMmFkY2I2M2UwOWYzMDAzNjIwZTVkZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZiDhx3l_TlGa-kltGCx2n93KEaIzVscss5jVqe7stwQ"
 }
 
 const axiosInstance = axios.create({
     baseURL: vars.REACT_APP_TMDB_BASE_URL_API,
-    params: { language: "es" },
+    params: { language: "es_LA" },
     headers: {
         accept: 'application/json',
         Authorization: `Bearer ${vars.TMDB_API_KEY}`
