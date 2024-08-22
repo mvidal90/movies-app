3b2ffb743c6a714d52d85fc546e4dc5a6f12d363
Marcos Vidal <markos_ari@hotmail.com>
2024-08-22 12:50:12 -0300

    ADD dinamic metadata

--- app/detail/[id]/page.tsx
+++ app/detail/[id]/page.tsx
@@ -1,5 +1,23 @@
 import { MovieDetail } from "@/components";
+import { getMovieById } from "@/util/api";
 import { Box, Container } from "@mui/material";
+
+import type { Metadata } from 'next'
+ 
+type Props = {
+  params: { id: string }
+}
+ 
+export async function generateMetadata(
+    { params }: Props,
+): Promise<Metadata> {
+    const movie = await getMovieById(params.id)
+    
+    return {
+        title: movie.title,
+        description: movie.overview
+    }
+}
 
 export default function Detail({params: {id}}: { params: { id: string }}) {
     return (
